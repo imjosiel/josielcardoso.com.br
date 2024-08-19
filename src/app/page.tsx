3d03d355f58191b3945f524dee0e2003922c3154
Josiel Cardoso <josielcardosomercio@gmail.com>
2024-08-19 12:44:20 -0300

    update: easybox url redefine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
         </a> */}
 
         <Link
-          href="https://barbershopcaixa.josielcardoso.com.br"
+          href="https://easybox.josielcardoso.com.br"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
